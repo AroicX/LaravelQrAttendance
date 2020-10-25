f149527b3916a870fb89a9a6401e65acc0c9261c
AroicX <Arowosegbe67@gmail.com>
2020-10-25 19:11:12 +0100

    ....

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,30 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
-            $table
-                ->string('course_id')
-                ->default([
-                    1,
-                    2,
-                    3,
-                    4,
-                    5,
-                    6,
-                    7,
-                    8,
-                    9,
-                    10,
-                    11,
-                    12,
-                    13,
-                    14,
-                    15,
-                    16,
-                    17,
-                    18,
-                    19,
-                    20,
-                ]);
+            $table->json('course_id')->nullable();
             $table->string('fullname');
             $table->string('email')->unique();
             $table->string('level')->nullable();
